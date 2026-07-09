d85b4dbe41e95300ccc72e5a0de6602362d4a014
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-09 20:51:49 +0530

    Added zod schemas

--- apps/http-backend/src/routes/auth.ts
+++ apps/http-backend/src/routes/auth.ts
@@ -1,11 +1,28 @@
 import { Router, type Router as ExpressRouter } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateUserSchema, SigninSchema } from "@repo/common/types";
 
 const authRouter: ExpressRouter = Router();
 
-authRouter.post("/signup", async (req, res) => {});
+authRouter.post("/signup", async (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
 
-authRouter.post("/signin", async (req, res) => {});
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
+});
+
+authRouter.post("/signin", async (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
+});
 
 export default authRouter;
--- apps/http-backend/src/routes/room.ts
+++ apps/http-backend/src/routes/room.ts
@@ -1,10 +1,17 @@
 import { Router, type Router as ExpressRouter } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { CreateRoomSchema } from "@repo/common/types";
 
 const roomRouter: ExpressRouter = Router();
 
 roomRouter.post("/create-room", authMiddleware, async (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
 
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
 });
 
 export default roomRouter;

190955cf52b25d156b209bcac03460517061a7fd
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-09 22:01:00 +0530

    Completed signup endpoint

--- apps/http-backend/src/middlewares/authMiddleware.ts
+++ apps/http-backend/src/middlewares/authMiddleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
     const header = req.headers["authorization"] as string;
--- apps/http-backend/src/routes/auth.ts
+++ apps/http-backend/src/routes/auth.ts
@@ -1,7 +1,9 @@
 import { Router, type Router as ExpressRouter } from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { CreateUserSchema, SigninSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const authRouter: ExpressRouter = Router();
 
@@ -13,6 +15,32 @@ authRouter.post("/signup", async (req, res) => {
       message: "Incorrect inputs",
     });
   }
+
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prismaClient.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hashedPassword,
+        name: name,
+      },
+    });
+  } catch (e) {
+    return res.status(400).json({
+      message: "Unable to signup",
+    });
+  }
+
+  res.json({
+    message: "You have been signed up successfully",
+  });
 });
 
 authRouter.post("/signin", async (req, res) => {

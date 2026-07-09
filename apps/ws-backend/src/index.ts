1cfac6b1c51d60efa98ebd6aae30b5e987b0ffb8
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-09 21:47:39 +0530

    Added prisma client

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,7 +4,7 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket, request) => {
+wss.on("connection", (ws, request) => {
   const url = request.url;
 
   if (!url) {
@@ -13,8 +13,14 @@ wss.on("connection", (socket, request) => {
 
   const queryParams = new URLSearchParams(url.split("?")[1]);
   const token = queryParams.get("token");
+  const decoded = jwt.verify(token as string, JWT_SECRET);
 
-  socket.on("message", (data) => {
-    socket.send("pong");
+  if (typeof decoded === "string") {
+    ws.close();
+    return;
+  }
+
+  ws.on("message", (data) => {
+    ws.send("pong");
   });
 });
